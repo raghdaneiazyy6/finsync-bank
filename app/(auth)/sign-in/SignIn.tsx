aa0a939fa5cfc2a472eb75bd4eef8388bb013308
Raghda Tarek Neiazy <raghda.neiazy@gmail.com>
2025-06-22 12:57:04 +0300

    implement initial file and folder stucture

---
+++ app/(auth)/sign-in/SignIn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingIn() {
+  return <div>Sign In</div>;
+}
+
+export default SingIn;
---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "FinSync",
+  description:
+    "FinSync Bank is a modern, secure platform to manage your finances effortlessly.",
+  icons: "/icons/logo.svg", //finsync logo
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
